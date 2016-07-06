80b0c35c5a6d863548e679de67c243b83ac67b26
ankurprasad711 <ankurprasad711@gmail.com>
2016-07-06 23:25:50 +0530

    my new calculator

--- javascrit.js
+++ javascrit.js
@@ -1,6 +1,12 @@
+var memory=0;
 var n;
 var m;
 var op;
+var char='.';
+setnumber1=function()
+{
+  document.getElementById('h').value+=char;
+}
 setnumber=function(arg1)
 {
 document.getElementById('h').value+=arg1;
@@ -25,7 +31,7 @@ play=function()
 {
   switch(op)
   {
-    case '+':m=n+parseFloat(document.getElementById('h').value);
+    case '+':m=(n+parseFloat(document.getElementById('h').value));
              break;
     case '/':m=n/parseFloat(document.getElementById('h').value);
               break;
@@ -34,6 +40,24 @@ play=function()
     case '*':m=n*parseFloat(document.getElementById('h').value);
               break;
   }
-    document.getElementById('h').value=m;
+  var j=m;
+    document.getElementById('h').value=j;
+
+}
+function memorysum()
+{
+
+  var t =parseFloat(memory)+ parseFloat(document.getElementById('h').value);
+  document.getElementById('h').value=t;
+}
+function memoryclear()
+{
+  document.getElementById('h').value=0;
+  memory=0;
+}
+function memory1()
+{
+  var s=document.getElementById('h').value;
+  memory=s;
 
 }
